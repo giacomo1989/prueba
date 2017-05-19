661c4d0bbe82b68872f5b975763e5bd365fe08eb
giacomo1989 <voltolina.giacomo89@gmail.com>
2017-05-19 10:15:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def processRequest(req):
 		
 		valore = data.get("price").get(pizza)
 		
-		speech = "ore 10.06 The price of pizza " +pizza+ " is "+valore+" euro. Bye Bye" 
+		speech = "ore 10.15 The price of pizza " +pizza+ " is "+valore+" euro. Bye Bye" 
 		res = makeWebhookResult(speech)
 		return res
 	
