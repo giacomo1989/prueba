0c75165331a43cc478d7ba32072902884efdc152
giacomo1989 <voltolina.giacomo89@gmail.com>
2017-06-15 16:57:08 +0200

    Create app.py

--- app.py
+++ app.py
@@ -32,6 +32,128 @@ def webhook():
 
 
 
+#url = "https://raw.githubusercontent.com/giacomo1989/prova-import/master/pizzaimport.json"
+#response = urllib.request.urlopen(url)
+#content = response.read()
+#data = json.loads(content.decode("utf8"))
+
+
+def processRequest(req):
+	if req.get("result").get("action") == "input.welcome":
+		result = req.get("result")
+		parameters = result.get("parameters")
+		saluto = parameters.get("benvenuto")
+		
+		speech = "sto facendo una prova per capire. il tuo saluto e'  stato " +saluto+" molto bene, lo legge " 
+		res = makeWebhookResult(speech)
+		return res
+		
+	elif req.get("result").get("action") == "order.pizza":
+		result = req.get("result")
+		parameters = result.get("parameters")
+		pizza = parameters.get("type")
+		#tonno = parameters.get("fish")
+		#cipolla = parameters.get("vegetables")
+		#if tonno == "tonno" and cipolla == "cipolla" :
+		speech = "10.22 perfect, your "+pizza+"it will be ready in 5 minuts. Bye Bye" 
+		res = makeWebhookResult(speech)
+		return res
+		#else
+		#	return {}
+	
+	elif req.get("result").get("action") == "adding":
+		result = req.get("result")
+		parameters = result.get("parameters")
+		#tonno= parameters.get("fish")
+		#aa=fish[0]
+		#if aa == "tonno" :
+		if parameters.get("type") == "margherita" :
+			if parameters.get("fish") == "tonno" and parameters.get("vegetables") == "cipolla" :
+				speech = "12.13 Perfect, you added these extra on your pizza "+parameters.get("type")+". your pizza tonno and cipolla will be ready in 5 minuts."
+				res = makeWebhookResult(speech)
+				return res
+			elif parameters.get("vegetables") == "cipolla" and parameters.get("fish") == "tonno" :
+				speech = "12.13 Perfect, you added these extra on your pizza "+parameters.get("type")+". your pizza tonno and cipolla will be ready in 5 minuts."
+				res = makeWebhookResult(speech)
+				return res
+		
+			else:
+				return{}
+		
+		elif parameters.get("type") == "diavola" :
+			if parameters.get("extra") != none : 
+				speech = "11.32 Perfect, your pizza "+parameters.get("type")+" with this extra: "+parameters.get("extra")+", "+parameters.get("fish")+", "+parameters.get("cheese")+", "+parameters.get("vegetables")+", "+parameters.get("embutido")+" it will be ready in 5 minuts."
+				res = makeWebhookResult(speech)
+				return res
+			else:
+				return{}
+		
+		else:
+			return{}
+	
+	else:
+		return {}
+
+def makeWebhookResult(speech):
+    print("Response:")
+    print(speech)
+
+    return {
+        "speech": speech,
+        "displayText": speech,
+        # "data":[],
+        # "contextOut": [],
+        "source": "prueba"
+    }
+
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+
+    print("Starting app on port %d" % port)
+
+    app.run(debug=False, port=port, host='0.0.0.0')
+
+
+
+
+'''
+CODICE CHE FUNZIONA PER L'APPLICAZIONE PIZZA.BCN IN API.AI
+
+#!/usr/bin/env python
+
+from __future__ import print_function
+from future import standard_library
+standard_library.install_aliases()
+import urllib.request, urllib.parse, urllib.error
+import json
+import os
+
+from flask import Flask
+from flask import request
+from flask import make_response
+
+# Flask app should start in global layout
+app = Flask(__name__)
+
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
+
+    print("Request:")
+    print(json.dumps(req, indent=4))
+
+    res = processRequest(req)
+
+    res = json.dumps(res, indent=4)
+    # print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+
+
 url = "https://raw.githubusercontent.com/giacomo1989/prova-import/master/pizzaimport.json"
 response = urllib.request.urlopen(url)
 content = response.read()
@@ -113,3 +235,4 @@ if __name__ == '__main__':
     print("Starting app on port %d" % port)
 
     app.run(debug=False, port=port, host='0.0.0.0')
+    '''
