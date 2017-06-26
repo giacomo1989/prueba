6ef3dd63caa9a9e669ff04e308f40061c73a7205
giacomo1989 <voltolina.giacomo89@gmail.com>
2017-06-26 11:38:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ def processRequest(req):
 			ass = 			req.get("result")["contexts"][2]["parameters"].get("assicurazione")
 			danni=			req.get("result")["contexts"][1]["parameters"].get("forniture-demage")
 
-			license = "Dear costumer, the claim of you car accident, with these details:\n-LICENSE PLATE NUMBER: "+licenseplate+"\n-DATE OF THE ACCIDENT: "+dateloss+"\n-TIME OF THE ACCIDENT: "+timeloss+"\n-PLACE OF THE ACCIDENT: "+cityloss+"\n, has been correct registered.\n\nPlease use claim no. 12345 for reference" 
+			license = "Dear costumer, the claim of you car accident, with these details:\n-LICENSE PLATE NUMBER: "+licenseplate+"\n-DATE OF THE ACCIDENT: "+dateloss+"\n-TIME OF THE ACCIDENT: "+timeloss+"\n-PLACE OF THE ACCIDENT: "+cityloss+"\nhas been correct registered.\n\nPlease use claim no. 12345 for reference" 
 			important="\n\n******** IMPORTANT ********\n\nThe schedule of the third part driver involved in the accident is:\n-NAME: "+name_other_driver+"\n-SURNAME: "+surname_other_driver+"\n-DATE OF BIRTH: "+datedriver2+"\n-LICENSE NUMBER: "+driver2_license_number+"\n-LICENSE PLATE NUMBER: "+driver2_license_plate+"\n-INSURANCE: "+ass
 			#funziona posizione=" posizione 0 e "+req.get("result")["contexts"][0]["name"]+"\nposizione 1 "+req.get("result")["contexts"][1]["name"]+ "posizione 2 "+req.get("result")["contexts"][2]["name"]+" facciamo prova e vediamo se alcuni dati inseriti vanno bene "+name_other_driver+" "+surname_other_driver+" "+cityloss
 						#SI POLIZIA
@@ -86,10 +86,10 @@ def processRequest(req):
 			part_injured = 		req.get("result")["contexts"][4]["parameters"].get("injury-part")
 			seat = 			req.get("result")["contexts"][4]["parameters"].get("seat-position")
 			
-			license = "Dear costumer, the claim of you car accident, with these details:\n-LICENSE PLATE NUMBER: "+licenseplate+"\n-DATE OF THE ACCIDENT: "+dateloss+"\n-TIME OF THE ACCIDENT: "+timeloss+"\n-PLACE OF THE ACCIDENT: "+cityloss+"\n, has been correct registered.\n\nPlease use claim no. 12345 for reference" 
+			license = "Dear costumer, the claim of you car accident, with these details:\n-LICENSE PLATE NUMBER: "+licenseplate+"\n-DATE OF THE ACCIDENT: "+dateloss+"\n-TIME OF THE ACCIDENT: "+timeloss+"\n-PLACE OF THE ACCIDENT: "+cityloss+"\nhas been correct registered.\n\nPlease use claim no. 12345 for reference" 
 			important="\n\n******** IMPORTANT ********\n\nThe schedule of the third part driver involved in the accident is:\n-NAME: "+name_other_driver+"\n-SURNAME: "+surname_other_driver+"\n-DATE OF BIRTH: "+datedriver2+"\n-LICENSE NUMBER: "+driver2_license_number+"\n-LICENSE PLATE NUMBER: "+driver2_license_plate+"\n-INSURANCE: "+ass
 			#injured=" vediamo se funziona."
-			injured="\n\n***** VERY IMPORTANT *****\n\nTHE PASSENGER:"+name_injured+" "+surname_injured+"; was injured in the "+part_injured+". "+name_injured+" found himself in "+seat+"."
+			injured="\n\n***** VERY IMPORTANT *****\n\nTHE PASSENGER: "+name_injured+" "+surname_injured+"; was injured in the "+part_injured+". "+name_injured+" found himself in "+seat+"."
 			#speech=" posizione 0 e "+req.get("result")["contexts"][0]["name"]+"\nposizione 1 "+req.get("result")["contexts"][1]["name"]+"\nposizione 2 "+req.get("result")["contexts"][2]["name"]+"\nposizione 3 "#+req.get("result")["contexts"][3]["name"]+"\nposizione 4 "+req.get("result")["contexts"][4]["name"]
 			#res = makeWebhookResult(speech)
 			#return res
@@ -98,7 +98,7 @@ def processRequest(req):
 				Ndenuncia = 	req.get("result")["contexts"][2]["parameters"].get("complain-number")
 				agentID = 	req.get("result")["contexts"][2]["parameters"].get("agent-id")
 				extra2="\nThe police have been called.The complain number: "+Ndenuncia+" taken by Agent "+agentID+", was properly loaded.\nThere were street forniture demages:\n"+danni
-				prova="11.51 "+license+important+injured+extra2
+				prova=license+important+injured+extra2
 				res = makeWebhookResult(prova)
 				return res
 						#NO POLIZIA
@@ -126,7 +126,7 @@ def processRequest(req):
 			driver2_license_plate = req.get("result")["contexts"][2]["parameters"].get("license-plate")
 			ass = 			req.get("result")["contexts"][2]["parameters"].get("assicurazione")
 
-			license = "Dear costumer, the claim of you car accident, with these details:\n-LICENSE PLATE NUMBER: "+licenseplate+"\n-DATE OF THE ACCIDENT: "+dateloss+"\n-TIME OF THE ACCIDENT: "+timeloss+"\n-PLACE OF THE ACCIDENT: "+cityloss+"\n, has been correct registered.\n\nPlease use claim no. 12345 for reference" 
+			license = "Dear costumer, the claim of you car accident, with these details:\n-LICENSE PLATE NUMBER: "+licenseplate+"\n-DATE OF THE ACCIDENT: "+dateloss+"\n-TIME OF THE ACCIDENT: "+timeloss+"\n-PLACE OF THE ACCIDENT: "+cityloss+"\nhas been correct registered.\n\nPlease use claim no. 12345 for reference" 
 			important="\n\n******** IMPORTANT ********\n\nThe schedule of the third part driver involved in the accident is:\n-NAME: "+name_other_driver+"\n-SURNAME: "+surname_other_driver+"\n-DATE OF BIRTH: "+datedriver2+"\n-LICENSE NUMBER: "+driver2_license_number+"\n-LICENSE PLATE NUMBER: "+driver2_license_plate+"\n-INSURANCE: "+ass
 
 			#speech=" posizione 0 e "+req.get("result")["contexts"][0]["name"]+"\nposizione 1 "+req.get("result")["contexts"][1]["name"]+"\nposizione 2 "+req.get("result")["contexts"][2]["name"]+"\nposizione 3 "#+req.get("result")["contexts"][3]["name"]+"\nposizione 4 "+req.get("result")["contexts"][4]["name"]
@@ -137,7 +137,8 @@ def processRequest(req):
 				Ndenuncia = 	req.get("result")["contexts"][1]["parameters"].get("complain-number")
 				agentID = 	req.get("result")["contexts"][1]["parameters"].get("agent-id")
 				extra2="\nThere were not injured.\nThe police have been called.\nThe complain number: "+Ndenuncia+" taken by Agent "+agentID+", was properly loaded.\nThere were not street forniture demages."
-				prova="11.51 "+str(len(req.get("result")["contexts"][1]["parameters"]))+license+important+extra2
+				#prova="11.51 "+str(len(req.get("result")["contexts"][1]["parameters"]))+license+important+extra2
+				prova=license+important+extra2
 				res = makeWebhookResult(prova)
 				return res
 						#NO POLIZIA
@@ -164,10 +165,10 @@ def processRequest(req):
 			part_injured = 		req.get("result")["contexts"][4]["parameters"].get("injury-part")
 			seat = 			req.get("result")["contexts"][4]["parameters"].get("seat-position")
 			
-			license = "Dear costumer, the claim of you car accident, with these details:\n-LICENSE PLATE NUMBER: "+licenseplate+"\n-DATE OF THE ACCIDENT: "+dateloss+"\n-TIME OF THE ACCIDENT: "+timeloss+"\n-PLACE OF THE ACCIDENT: "+cityloss+"\n, has been correct registered.\n\nPlease use claim no. 12345 for reference" 
+			license = "Dear costumer, the claim of you car accident, with these details:\n-LICENSE PLATE NUMBER: "+licenseplate+"\n-DATE OF THE ACCIDENT: "+dateloss+"\n-TIME OF THE ACCIDENT: "+timeloss+"\n-PLACE OF THE ACCIDENT: "+cityloss+"\nhas been correct registered.\n\nPlease use claim no. 12345 for reference" 
 			important="\n\n******** IMPORTANT ********\n\nThe schedule of the third part driver involved in the accident is:\n-NAME: "+name_other_driver+"\n-SURNAME: "+surname_other_driver+"\n-DATE OF BIRTH: "+datedriver2+"\n-LICENSE NUMBER: "+driver2_license_number+"\n-LICENSE PLATE NUMBER: "+driver2_license_plate+"\n-INSURANCE: "+ass
 			#injured=" vediamo se funziona."
-			injured="\n\n***** VERY IMPORTANT *****\n\nTHE PASSENGER:"+name_injured+" "+surname_injured+"; was injured in the "+part_injured+". "+name_injured+" found himself in "+seat+"."
+			injured="\n\n***** VERY IMPORTANT *****\n\nTHE PASSENGER: "+name_injured+" "+surname_injured+"; was injured in the "+part_injured+". "+name_injured+" found himself in "+seat+"."
 			#speech=" posizione 0 e "+req.get("result")["contexts"][0]["name"]+"\nposizione 1 "+req.get("result")["contexts"][1]["name"]+"\nposizione 2 "+req.get("result")["contexts"][2]["name"]+"\nposizione 3 "#+req.get("result")["contexts"][3]["name"]+"\nposizione 4 "+req.get("result")["contexts"][4]["name"]
 			#res = makeWebhookResult(speech)
 			#return res
@@ -176,7 +177,7 @@ def processRequest(req):
 				Ndenuncia = 	req.get("result")["contexts"][2]["parameters"].get("complain-number")
 				agentID = 	req.get("result")["contexts"][2]["parameters"].get("agent-id")
 				extra2="\nThe police have been called. The complain number: "+Ndenuncia+" taken by Agent "+agentID+", was properly loaded.\nThere were not street forniture demages."
-				prova="11.51 "+license+important+injured+extra2
+				prova=license+important+injured+extra2
 				res = makeWebhookResult(prova)
 				return res
 						#NO POLIZIA
