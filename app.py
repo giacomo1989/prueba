ff6b640e38ff12ddabfcdacbea0d6e51517293d1
giacomo1989 <voltolina.giacomo89@gmail.com>
2017-06-27 11:43:13 +0200

    Update app.py

--- app.py
+++ app.py
@@ -215,7 +215,7 @@ def processRequest(req):
 		
 		#result = req.get("result")
 		#parameters = result.get("parameters")
-		speech="ok il collegamento e attivo "
+		speech="ok il collegamento e attivo "+lat
 		res = makeWebhookResult(speech)
 		return res
 	
