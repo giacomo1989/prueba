3c08388f1f3d4075c2899aa561a0b0cca03b8f98
giacomo1989 <voltolina.giacomo89@gmail.com>
2017-06-27 16:14:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ import urllib.request, urllib.parse, urllib.error
 import json
 import os
 
-import urllib
 from flask import abort
 import urlfetch
 
@@ -208,7 +207,7 @@ def processRequest(req):
 		
 		#result = req.get("result")
 		#parameters = result.get("parameters")
-		speech="ok il collegamento e attivo "#+lat
+		speech="16.13 ok il collegamento e attivo "#+lat
 		res = makeWebhookResult(speech)
 		return res
 	
