ebb2c58ebce6b0dcb49c12a301246cb50362fb8e
giacomo1989 <voltolina.giacomo89@gmail.com>
2017-06-28 11:37:12 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from __future__ import print_function
 from future import standard_library
 standard_library.install_aliases()
 import urllib.request, urllib.parse, urllib.error
-import urllib2
+from urllib.request import urlopen
 import json
 import os
 
