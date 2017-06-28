8a5e39b5512977af525e3c9415d0d90a60d146ed
giacomo1989 <voltolina.giacomo89@gmail.com>
2017-06-28 11:42:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -334,9 +334,9 @@ def processRequest(req):
 		yql_query = "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text= '"+ city+"')"
 		yql_url = baseurl + urllib.urlencode({'q':yql_query}) + "&format=json"
 		result1 = urlopen(yql_url).read()
-    		#data = json.loads(result1)
+    		data = json.loads(result1)
 		
-		speech="11.40 le previsioni meteo a "+city+" saranno a dispozione a breve"
+		speech="11.42 le previsioni meteo a "+city+" saranno a dispozione a breve"
 		res = makeWebhookResult(speech)
 		return res
 		
