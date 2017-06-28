2e8173e38885c3b73aa743554b4d29692a8181dd
giacomo1989 <voltolina.giacomo89@gmail.com>
2017-06-28 12:21:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -347,9 +347,9 @@ def processRequest(req):
 		#simbolo_gradi=units.get('temperature')
 		
 		celsius=condition.get('temp')
-		gc= int(celsius)
+		#gc= int(celsius)
        		#speech1 = "12.05 Today in " + location.get('city') + ": " + condition.get('text') +", the temperature is " + str(celsius) + " C"
-		speech1 = gc+" 12.14 Today in " + location.get('city') + ": " + condition.get('text') + ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
+		speech1 = " 12.21 Today in " + location.get('city') + ": " + condition.get('text') + ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
 		
 		speech="11.42 le previsioni meteo a "+city+" saranno a dispozione a breve"
 		res = makeWebhookResult(speech1)
