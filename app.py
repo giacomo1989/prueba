2b01acc6d39e3d1dcbde4a46ca5f5976eaad6ef1
giacomo1989 <voltolina.giacomo89@gmail.com>
2017-06-29 17:59:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -221,11 +221,15 @@ def processRequest(req):
 		#parameters = result.get("parameters")
 		
 		aa="https://graph.facebook.com/v2.6/<USER_ID>?fields=first_name,last_name,profile_pic,locale,timezone,gender&access_token=EAAODZBYcpPmkBAEj9EVraapZA3US5ZCo9A084X8AT8hqOiPRcUpecq7SLzEyvJKbibIjn8nLTtvUwCBmLOJQu7j8nIUEVGYX9D94PkDJ50ZCT5k0wUguYNQx3zgvs9ZATHmxOwFXn5snFR10rnwdxKXsyHdGV7bUXkYgrzWecMgZDZD"
+		result2 = urlopen(aa).read()
+		data = json.loads(result2)
+		
+		
 		baseurl ="https://graph.facebook.com/v2.6/me/subscribed_apps?access_token=EAAODZBYcpPmkBAEj9EVraapZA3US5ZCo9A084X8AT8hqOiPRcUpecq7SLzEyvJKbibIjn8nLTtvUwCBmLOJQu7j8nIUEVGYX9D94PkDJ50ZCT5k0wUguYNQx3zgvs9ZATHmxOwFXn5snFR10rnwdxKXsyHdGV7bUXkYgrzWecMgZDZD"
 		result1 = urlopen(baseurl).read()
 		data = json.loads(result1)
 		
-		#nome=data.get("first_name")
+		nome=data.get("first_name")
 		
 		speech="17.58 ok il collegamento e attivo "#+lat
 		res = makeWebhookResult(speech)
