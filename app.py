5f6e54d856e1520fc37451639fa87e1e19ba0b9d
giacomo1989 <voltolina.giacomo89@gmail.com>
2017-07-04 14:05:13 +0200

    Update app.py

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ def processRequest(req):
 			danni=			req.get("result")["contexts"][1]["parameters"].get("forniture-demage")
 			testo=			req.get("result")["contexts"][1]["parameters"].get("any")
 			
-			license = day+", "+date+"\n\nDear costumer this is the REPORT of you car accident:\n-LICENSE PLATE NUMBER: "+licenseplate+"\n-DATE OF THE ACCIDENT: "+dateloss+"\n-TIME OF THE ACCIDENT: "+timeloss+"\n-PLACE OF THE ACCIDENT: "+cityloss+"\nhas been correct registered.\n\nPlease use claim no. 12345 for reference" 
+			license = "THANK YOU FOR YOUR TIME, THIS IS YOUR FINAL REPORT.\nMAKE SURE THAT ALL THE INFORMATION IS CORRECT\n\n\n"+day+", "+date+"\n\nDear costumer this is the REPORT of you car accident:\n-LICENSE PLATE NUMBER: "+licenseplate+"\n-DATE OF THE ACCIDENT: "+dateloss+"\n-TIME OF THE ACCIDENT: "+timeloss+"\n-PLACE OF THE ACCIDENT: "+cityloss+"\nhas been correct registered.\n\nPlease use claim no. 12345 for reference" 
 			important="\n\n******** IMPORTANT ********\n\nThe schedule of the third part driver involved in the accident is:\n-NAME: "+name_other_driver+"\n-SURNAME: "+surname_other_driver+"\n-DATE OF BIRTH: "+datedriver2+"\n-LICENSE NUMBER: "+driver2_license_number+"\n-LICENSE PLATE NUMBER: "+driver2_license_plate+"\n-INSURANCE: "+ass
 			#funziona posizione=" posizione 0 e "+req.get("result")["contexts"][0]["name"]+"\nposizione 1 "+req.get("result")["contexts"][1]["name"]+ "posizione 2 "+req.get("result")["contexts"][2]["name"]+" facciamo prova e vediamo se alcuni dati inseriti vanno bene "+name_other_driver+" "+surname_other_driver+" "+cityloss
 						#SI POLIZIA
